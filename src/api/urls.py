283ad042914ea2fbaad4661d8d13752799e808c7
Lysenko Volodymyr <mr.volodumyr@gmail.com>
2026-06-07 12:46:52 +0200

    Solution1

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,11 +4,13 @@ from rest_framework.routers import DefaultRouter
 from api import views
 
 router = DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"todolists", views.TodoListViewSet)
-router.register(r"todos", views.TodoViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'lists', views.TodoListViewSet)
+router.register(r'todos', views.TodoViewSet)
 
-app_name = "api"
 urlpatterns = [
-    path("", include(router.urls))
+    path('', include(router.urls)),  # Зверни увагу на кому в кінці рядка!
+
+    path('healthz/liveness/', views.liveness_probe, name='liveness'),
+    path('healthz/readiness/', views.readiness_probe, name='readiness'),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -4,9 +4,16 @@ from rest_framework import permissions, viewsets
 from api.serializers import TodoListSerializer, TodoSerializer, UserSerializer
 from lists.models import Todo, TodoList
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 import time
+
+def liveness_probe(request):
+    return JsonResponse({"status": "alive"}, status=200)
+
+def readiness_probe(request):
+    return JsonResponse({"status": "ready"}, status=200)
+
 
 class IsCreatorOrReadOnly(permissions.BasePermission):
     """
@@ -15,28 +22,22 @@ class IsCreatorOrReadOnly(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # If the object doesn't have a creator (i.e. anon) allow all methods.
         if not obj.creator:
             return True
 
-        # Instance must have an attribute named `creator`.
         return obj.creator == request.user
 
 
 class UserViewSet(viewsets.ModelViewSet):
-
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (permissions.IsAdminUser,)
 
 
 class TodoListViewSet(viewsets.ModelViewSet):
-
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
     permission_classes = (IsCreatorOrReadOnly,)
@@ -46,8 +47,8 @@ class TodoListViewSet(viewsets.ModelViewSet):
         creator = user if user.is_authenticated else None
         serializer.save(creator=creator)
 
-class TodoViewSet(viewsets.ModelViewSet):
 
+class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     permission_classes = (IsCreatorOrReadOnly,)
